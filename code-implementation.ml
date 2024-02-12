f7820d6e20c18eea816a025d88fe225f5b1e7052
Zheng Robert Jia <jia00129@umn.edu>
2024-02-12 12:26:44 -0600

    Feb12

--- code-implementation.ml
+++ code-implementation.ml
@@ -140,8 +140,8 @@ type ('key,'value) bst =
 let bstGet root key = 
   let rec bstGetting subtree = 
     match subtree with 
-      BSTEmpty -> raise BstError "No such key."|
-      BstNode (otherKey, otherValue, left, riht) -> 
+      BSTEmpty -> raise (BstError "No such key.")|
+      BSTNode (otherKey, otherValue, left, right) -> 
         if key < otherKey
           then bstGetting left 
         else if key > otherKey
@@ -156,16 +156,16 @@ let bstGet root key =
 let bstPut root key value= 
   let rec bstPutting subtree = 
     match subtree with
-      BSTEmpty -> BstNode(key,value,BSTEmpty,BSTEmpty)|
-      BstNode (otherKey,otherValue,left,right)->
+      BSTEmpty -> BSTNode(key,value,BSTEmpty,BSTEmpty)|
+      BSTNode (otherKey,otherValue,left,right)->
         if key < otherKey
-          then BstNode(otherKey,otherValue,(bstPutting left),right) (* This is not tail recursive*)
+          then BSTNode(otherKey,otherValue,(bstPutting left),right) (* This is not tail recursive*)
         else if key > otherKey
-          then BstNode(otherKey,otherValue, left, (bstPutting right)) (* This is not tail recursive*)
+          then BSTNode(otherKey,otherValue, left, (bstPutting right)) (* This is not tail recursive*)
         else if value = otherValue
               then subtree
-            else BstNode(key, value, left, right)
-  in bstPuting root;;
+            else BSTNode(key, value, left, right)
+  in bstPutting root;;
 
 (* building a BST *)
 (* let t = BSTEmpty;;
@@ -174,20 +174,22 @@ let t = bstPut t 7 "seven";;
 let t = bstPut t 7 "seven";;
 let t = bstPut t 15 "fifteen";; *)
 
-let t = BSTEmpty in 
+(* let t = BSTEmpty in 
   let t = bstPut t 10 "ten" in 
     let t = bstPut t 7 "seven" in 
-      let t = bstPut t 15 "fifteen" ;;
+      let t = bstPut t 15 "fifteen" ;; *)
 
 let rec isBstEqual left right = 
   match (left,right) with 
     (BSTEmpty,BSTEmpty) -> true|
-    (BSTEmpty,BstNode (_,_,_,_)) |(BstNode (_,_,_,_), BSTEmpty) -> false | 
-    (BstNode (l_k,_,left_left,left_right),BstNode (r_k,_,right_left, right_right)) -> 
+    (BSTEmpty,BSTNode (_,_,_,_)) |(BSTNode (_,_,_,_), BSTEmpty) -> false | 
+    (BSTNode (l_k,_,left_left,left_right),BSTNode (r_k,_,right_left, right_right)) -> 
       if l_k <> r_k && 
-        isBstEqual (left_left,right_left) &&  (* not tail recursive*)
-        isBstEqual (left_right,right_right);; (* tail recursive*)
+        (isBstEqual left_left right_left) &&  (* not tail recursive*)
+        (isBstEqual left_right right_right) then true (* tail recursive*)
+      else false;; 
 
+        
 let k = 1;;
 let f() = k;;
 let k = 2 in f();; (* return 1*)
@@ -197,3 +199,80 @@ let k = 2 in f();; (* return 1*)
 (* function is closed -> remmebers its defining environment (static binding/lexical binding)*)
 
 (* dynamic binding: looks in current environment instead of defining environment  *)
+
+let rest = 2;;
+let plusRest n = n+rest;; (* closed in defining environment *)
+(* Function line plusRest is represented as an objected called a closure *)
+(* closure: 
+   [     ] environment
+   [     ] parameters 
+   [     ] body (code)*)
+
+(* 
+  plusRest: 
+   [     ] environment    <- rest = 2; (+) = [code??]; 
+   [     ] parameters     <- n
+   [     ] body (code)    <- n + rest
+   *)
+
+   (* (+) 2 3 -> 5
+      ( * ) 2 3 -> 6 *)
+
+
+(* Partial applications / "curried" functions 
+   Combinatory logic *)
+
+let add n m = n+m;;
+let add_two = add 2;;
+
+(* logic: 
+   0-order logic: no predicates 
+   1-order logic: predicates that can be applied to objects other than predcates P(x)
+   2-order logic: predicates that can be applied to other predicates R(P,Q) ??????????????? *)
+
+
+
+let map func objects = 
+  let rec mapping objects = 
+    match objects with 
+      [] -> [] | 
+      first::rest -> 
+        (func first) :: (mapping rest)
+  in mapping objects;;
+  (* ('a -> 'b) -> 'a list -> 'b list*) 
+
+let temp = (fun n -> n+1);;
+let temp = map (fun n -> n+1) [1;2;3];;
+
+let filter isKept things = 
+  let rec filtering things = 
+    match things with 
+      [] -> []|
+      firstThing::otherThings -> 
+        if isKept firstThing then
+          firstThing::(filtering otherThings)
+      else filtering otherThings
+  in filtering things;;
+
+let temp = filter (fun n -> n mod 2 = 1) [0;1;2;3;4;5];;
+
+let isOdd n = (n mod 2 = 1);;
+let opposite hasProperty = (fun things -> not (hasProperty things));;
+
+
+(* Continuation passing style (CPS) *)
+(* function that returns 0,1,2,3,... values  *)
+(* generate values 1 at a time *)
+
+(* In CPS, a functino doesn't return a useful value (It can)
+   but calls another function ---- called its continuation *)
+
+
+let generateBools etc n = 
+  let rec generating bools n = 
+    match n with 
+      0 -> etc bools | 
+      _ -> 
+        generating (false::bools) (n-1);
+        generating (true::bools) (n-1)
+  in generating [] n
